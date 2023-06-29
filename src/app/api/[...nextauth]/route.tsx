6de1b96967dc2803196e4ed8be5a92f197891467
Samuel Torres <rilla@MacBook-Pro.home>
2023-06-29 14:57:10 -0400

    build error fix

--- src/app/api/[...nextauth]/route.tsx
+++ src/app/api/[...nextauth]/route.tsx
@@ -1,5 +1,4 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google"
 
 interface Auth {
@@ -7,18 +6,13 @@ interface Auth {
     clientSecret: string;
 }
 
-export const authOptions = {
-  // Configure one or more authentication providers
+const handler = NextAuth({
   providers: [
-    // GithubProvider({
-    //   clientId: process.env.GITHUB_ID,
-    //   clientSecret: process.env.GITHUB_SECRET,
-    // }),
     GoogleProvider({
         clientId: process.env.GOOGLE_CLIENT_ID as Auth["clientId"],
         clientSecret: process.env.GOOGLE_CLIENT_SECRET as Auth["clientSecret"],
       }),
-    // ...add more providers here
   ],
-}
-export default NextAuth(authOptions)
+})
+
+export { handler as GET, handler as POST};
