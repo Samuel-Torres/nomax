e7f75831e2f9fab23daefd78cb0c2f0da8918573
Samuel Torres <rilla@MacBook-Pro.local>
2023-06-30 17:02:50 -0400

    comment out seed data

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,57 +1,57 @@
-import { Prisma, PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
+// import { Prisma, PrismaClient } from "@prisma/client";
+// const prisma = new PrismaClient();
 
-async function main() {
-  const users = [
-    {
-      email: "first@gmail.com",
-      password: "123",
-      created_At: new Date(),
-      bio: "New Bio",
-      persona: "PASSPORTBRO",
-      jobTitle: "Software Engineer",
-      companyName: "Tech",
-    },
-    {
-      email: "second@gmail.com",
-      password: "123",
-      created_At: new Date(),
-      bio: "I'm new here",
-      persona: "EXPAT",
-      jobTitle: "Data Analyst",
-      companyName: "Tech World",
-    },
-    {
-      email: "third@gmail.com",
-      password: "123",
-      created_At: new Date(),
-      bio: "Another one",
-      persona: "BACKPACKER",
-      jobTitle: "Coach Potato",
-      companyName: "Your Coach",
-    },
-    {
-      email: "fourth@gmail.com",
-      password: "123",
-      created_At: new Date(),
-      bio: "Hello Governor",
-      persona: "DIGITALNOMAD",
-      jobTitle: "Sales",
-      companyName: "Your Moms House",
-    },
-  ];
-  users.map(async (user) => {
-    await prisma.users.create({
-      data: user,
-    });
-  });
-}
+// async function main() {
+//   const users: Prisma.UsersCreateInput[] = [
+//     {
+//       email: "first@gmail.com",
+//       password: "123",
+//       created_At: new Date(),
+//       bio: "New Bio",
+//       persona: "PASSPORTBRO",
+//       jobTitle: "Software Engineer",
+//       companyName: "Tech",
+//     },
+//     {
+//       email: "second@gmail.com",
+//       password: "123",
+//       created_At: new Date(),
+//       bio: "I'm new here",
+//       persona: "EXPAT",
+//       jobTitle: "Data Analyst",
+//       companyName: "Tech World",
+//     },
+//     {
+//       email: "third@gmail.com",
+//       password: "123",
+//       created_At: new Date(),
+//       bio: "Another one",
+//       persona: "BACKPACKER",
+//       jobTitle: "Coach Potato",
+//       companyName: "Your Coach",
+//     },
+//     {
+//       email: "fourth@gmail.com",
+//       password: "123",
+//       created_At: new Date(),
+//       bio: "Hello Governor",
+//       persona: "DIGITALNOMAD",
+//       jobTitle: "Sales",
+//       companyName: "Your Moms House",
+//     },
+//   ];
+//   users.map(async (user) => {
+//     await prisma.users.create({
+//       data: user,
+//     });
+//   });
+// }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-  .catch(async (e) => {
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect();
+//   })
+//   .catch(async (e) => {
+//     await prisma.$disconnect();
+//     process.exit(1);
+//   });
