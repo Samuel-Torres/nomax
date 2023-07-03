5ddafb590b24dd15b896a1f3caeafec80d73d1be
Samuel Torres <rilla@MacBook-Pro.home>
2023-07-03 13:51:06 -0400

    comment out wrong users endpoint

--- src/app/api/users/route.tsx
+++ src/app/api/users/route.tsx
@@ -1,17 +1,17 @@
-import { NextResponse, NextRequest } from "next/server";
-import { PrismaClient } from "@prisma/client";
-import { getSession } from "next-auth/client";
+// import { NextResponse, NextRequest } from "next/server";
+// import { PrismaClient } from "@prisma/client";
+// import { getSession } from "next-auth/client";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-export const GET = async (req: NextRequest, res: NextResponse) => {
-  const session = await getSession({ req });
-  const email: string = req.email;
-  try {
-    const user = await prisma.users.findUnique({
-      where: {
-        email: email,
-      },
-    });
-  } catch (error) {}
-};
+// export const GET = async (req: NextRequest, res: NextResponse) => {
+//   const session = await getSession({ req });
+//   const email: string = req.email;
+//   try {
+//     const user = await prisma.users.findUnique({
+//       where: {
+//         email: email,
+//       },
+//     });
+//   } catch (error) {}
+// };
