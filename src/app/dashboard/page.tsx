f324f92c4b60be77381844a373e8b977c0c04708
Samuel Torres <rilla@MacBook-Pro.home>
2023-07-03 21:23:06 -0400

    Finished fetching user data on the client dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,19 @@
+"use client";
+import { useSession } from "next-auth/react";
+import useSWR from "swr";
+
 export default function Dashboard() {
+  const session = useSession();
+  const fetcher = (...args: string[]): Promise<any> =>
+    fetch(args.join(",")).then((res) => res.json());
+
+  const { data, mutate, error, isLoading } = useSWR(
+    `/api/loggedInUser/${session.data?.user?.email}`,
+    fetcher
+  );
+
+  console.log("FETCHED CLIENT DATA: ", data);
+
   return (
     <div>
       <h1>DASHBOARD PAGE</h1>
