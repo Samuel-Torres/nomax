8146d4f5de876b53502f67551394ceb8a7a7f029
Samuel Torres <rilla@MacBook-Pro.home>
2023-07-18 19:59:57 -0400

    Started creation of the homepage hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,15 @@ import styles from "./page.module.scss";
 import Navbar from "../components/navbar/navbar";
 import HomePageHero from "@/components/homePageHero/homePageHero";
 
+// Data:
+import { homePageHeroData } from "@/staticData";
+
 export default function Home() {
-  console.log("ENV VAR:", process.env.DATABASE_URL_NON_POOLING);
   return (
     <main>
       <Navbar />
       <div className={styles.container}>
-        <h1>MAIN PAGE</h1>
-        <HomePageHero />
+        <HomePageHero {...homePageHeroData} />
       </div>
     </main>
   );
---
+++ src/staticData/index.ts
@@ -0,0 +1,29 @@
+export const homePageHeroData = {
+  heroImage:
+    "https://images.pexels.com/photos/1015568/pexels-photo-1015568.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+  alt: "hero",
+  header: "Creating Communities Abroad!",
+  icons: [
+    {
+      id: 0,
+      iconImage:
+        "https://res.cloudinary.com/dvz91qyth/image/upload/v1689722002/Nomex/landing%20page%20assets/homePageHero/viral-marketing_klgrew.png",
+      alt: "community",
+      text: "Find your friends circles no matter where in the world you are!",
+    },
+    {
+      id: 1,
+      iconImage:
+        "https://res.cloudinary.com/dvz91qyth/image/upload/v1689722002/Nomex/landing%20page%20assets/homePageHero/viral-marketing_klgrew.png",
+      alt: "community",
+      text: "Find your friends circles no matter where in the world you are!",
+    },
+    {
+      id: 2,
+      iconImage:
+        "https://res.cloudinary.com/dvz91qyth/image/upload/v1689722002/Nomex/landing%20page%20assets/homePageHero/viral-marketing_klgrew.png",
+      alt: "community",
+      text: "Find your friends circles no matter where in the world you are!",
+    },
+  ],
+};
