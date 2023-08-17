8fc47131de779a2f6c1db9a527911335364f643a
Samuel Torres <rilla@MacBook-Pro.home>
2023-08-17 19:10:50 -0400

    add cypress start component aka unit testing

---
+++ src/components/homePageHero/homePageHeroHomePageHero.cy.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import HomePageHero from "./homePageHero";
+
+describe("<HomePageHero />", () => {
+  it("renders", () => {
+    cy.fixture("homePageHero/homePageHero.json").then((homePageHeroData) => {
+      cy.intercept(homePageHeroData.heroImage, {
+        fixture:
+          "https://res.cloudinary.com/dvz91qyth/image/upload/v1689879602/Nomex/landing%20page%20assets/homePageHero/My_project-1_3_pt78yu.png",
+      }).as("heroImageRequest");
+
+      // console.log("THE DATA: ", homePageHeroData);
+      cy.mount(<HomePageHero {...homePageHeroData} />);
+      cy.wait("@heroImageRequest");
+    });
+  });
+});
