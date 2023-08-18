90a9149256f6b0e160edfef540570afef76cd37d
Samuel Torres <rilla@MacBook-Pro.home>
2023-08-17 23:43:17 -0400

    Fix breaking test issue

--- src/components/homePageHero/homePageHeroHomePageHero.cy.tsx
+++ src/components/homePageHero/homePageHeroHomePageHero.cy.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import HomePageHero from "./homePageHero";
 
 describe("<HomePageHero />", () => {
-  cy.fixture("homePageHero/homePageHero.json").then((homePageHeroData) => {
-    cy.intercept(homePageHeroData.heroImage, {
-      fixture:
-        "https://res.cloudinary.com/dvz91qyth/image/upload/v1689879602/Nomex/landing%20page%20assets/homePageHero/My_project-1_3_pt78yu.png",
-    }).as("heroImageRequest");
+  it("Home Page Hero Renders", () => {
+    cy.fixture("homePageHero/homePageHero.json").then((homePageHeroData) => {
+      cy.intercept(homePageHeroData.heroImage, {
+        fixture:
+          "https://res.cloudinary.com/dvz91qyth/image/upload/v1689879602/Nomex/landing%20page%20assets/homePageHero/My_project-1_3_pt78yu.png",
+      }).as("heroImageRequest");
 
-    // console.log("THE DATA: ", homePageHeroData);
-    cy.mount(<HomePageHero {...homePageHeroData} />);
-    cy.wait("@heroImageRequest");
+      // console.log("THE DATA: ", homePageHeroData);
+      cy.mount(<HomePageHero {...homePageHeroData} />);
+      cy.wait("@heroImageRequest");
+    });
   });
 });
