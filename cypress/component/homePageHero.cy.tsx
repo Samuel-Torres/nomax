0a5573bfeca91a9320e25615bf5c78508ea38749
Samuel Torres <rilla@MacBook-Pro.local>
2023-08-21 18:02:19 -0400

    Worked with Dustin Graham to fix testing bug

--- cypress/component/homePageHero.cy.tsx
+++ cypress/component/homePageHero.cy.tsx
@@ -12,16 +12,20 @@ describe("<HomePageHero />", () => {
 
       cy.get<HTMLImageElement>("[data-test=heroImg]")
         .should("be.visible")
-        .should(([img]) => {
+        .and(([img]) => {
           expect(img.naturalWidth).to.equal(1080);
         });
 
       cy.get<HTMLImageElement>("[data-test=iconImg]")
-        .should((el) => {
+        .should("be.visible")
+        .and((el) => {
           expect(el).to.have.length(3);
         })
-        .should(([images]) => {
-          [images].forEach((img) => expect(img.naturalWidth).to.equal(512));
+        .each((image) => {
+          const img = image.get(0) as HTMLImageElement;
+          cy.wrap(img, { timeout: 5000 })
+            .invoke("outerWidth")
+            .should("be.gt", 0);
         });
     });
   });
