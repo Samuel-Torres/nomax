aa9b4dd551bdc57733701a574ef0b9694c4627af
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2023-09-01 12:14:29 -0400

    add testing domain to to cors policy

--- src/setCommonCorsHeaders.ts
+++ src/setCommonCorsHeaders.ts
@@ -2,7 +2,7 @@ export default function setCommonCorsHeaders(origin: string | null) {
     const headers = new Headers();
     if(origin === "https://nomax.vercel.app") {
         headers.set('Access-Control-Allow-Origin', 'https://nomax.vercel.app');
-        headers.append('Access-Control-Allow-Origin', 'https://nomax-git-testing-onboardingform-rilladubz.vercel.app')
+        headers.set('Access-Control-Allow-Origin', 'https://nomax-git-testing-onboardingform-rilladubz.vercel.app')
     }
     headers.set('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH');
     headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
