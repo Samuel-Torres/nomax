1b2714e6e0fcba0dbfbfa2ee68680240477c268f
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2023-09-01 11:38:42 -0400

    Fix for server side requests

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -8,9 +8,12 @@ import { Posts } from "@prisma/client";
 import DashboardComponent from "@/components/dashboardComponent/dashboardComponent";
 
 async function getAllPosts() {
-  const response = await fetch("/api/posts", {
-    cache: "no-store",
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/posts`,
+    {
+      cache: "no-store",
+    }
+  );
 
   if (!response.ok) {
     throw new Error("Failed to fetch data");
