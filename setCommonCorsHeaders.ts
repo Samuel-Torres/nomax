f3f1ca4efb7fdfd6efce95545168d71dbaf5a423
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2023-09-01 13:06:39 -0400

    moved file to stop breakage

--- setCommonCorsHeaders.ts
+++ setCommonCorsHeaders.ts
@@ -1,7 +1,4 @@
-// export default function setCommonCorsHeaders() {
-//     const headers = new Headers();
-//     headers.set('Access-Control-Allow-Origin', 'https://nomax.vercel.app');
-//     headers.set('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH');
-//     headers.set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     return headers;
-// }
+
+export default function setCommonCorsHeaders() {
+
+}
