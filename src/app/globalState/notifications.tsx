957dc2275810e3fc279c3a277e4610ac08262e9c
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2023-09-28 11:48:15 -0400

    localStorage bug fix

--- src/app/globalState/notifications.tsx
+++ src/app/globalState/notifications.tsx
@@ -1,13 +1,19 @@
 "use client";
+import { useState, useEffect } from "react";
 import useSWR from "swr";
 import axios from "axios";
 import { useLoggedInUser } from "./user";
 
 export const useNotifications = () => {
-  let loggedInUserId;
-  if (typeof window !== "undefined") {
-    loggedInUserId = localStorage.getItem("athUsr") as string;
-  }
+  // const [loggedInUserId, setLoggedInUserId] = useState<string>();
+  const { id: loggedInUserId } = useLoggedInUser();
+
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     setLoggedInUserId(localStorage.getItem("athUsr") as string);
+  //   }
+  // }, [setLoggedInUserId]);
+
   console.log("ID: ", loggedInUserId);
   const fetcher = (url: string) =>
     axios
--- src/app/globalState/user.tsx
+++ src/app/globalState/user.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 import { useSession } from "next-auth/react";
 
 export const useLoggedInUser = () => {
-  // const [storedId, setStoredId] = useState<string>("");
+  const [loggedInUserId, setLoggedInUserId] = useState<string>();
   const { data: session, status } = useSession();
   const router = useRouter();
   const fetcher = async (url: string) => await fetch(url).then((r) => r.json());
@@ -21,22 +21,43 @@ export const useLoggedInUser = () => {
     router.push("/auth");
   }
 
-  const storedId: string = localStorage.getItem("athUsr") as string;
+  // const storedId: string = localStorage.getItem("athUsr") as string;
 
-  if (
-    !storedId &&
-    data &&
-    status !== "loading" &&
-    status === "authenticated" &&
-    typeof window !== "undefined"
-  ) {
-    localStorage.setItem("athUsr", data?.fetchedUser.id);
-  }
+  // useEffect(() => {
+  //   if (typeof window !== "undefined") {
+  //     setLoggedInUserId(localStorage.getItem("athUsr") as string);
+  //   }
+  // }, [setLoggedInUserId]);
+
+  // if (
+  //   !storedId &&
+  //   data &&
+  //   status !== "loading" &&
+  //   status === "authenticated" &&
+  //   typeof window !== "undefined"
+  // ) {
+  //   localStorage.setItem("athUsr", data?.fetchedUser.id);
+  // }
+
+  useEffect(() => {
+    const storedId: string = localStorage.getItem("athUsr") as string;
+    if (
+      !storedId &&
+      status !== "loading" &&
+      status === "authenticated" &&
+      typeof window !== "undefined"
+    ) {
+      localStorage.setItem("athUsr", data?.fetchedUser.id);
+      setLoggedInUserId(localStorage.getItem("athUsr") as string);
+    }
+    setLoggedInUserId(localStorage.getItem("athUsr") as string);
+    // localStorage.setItem("athUsr", data?.fetchedUser.id);
+  }, [setLoggedInUserId, data, status]);
 
   return {
     user: data?.fetchedUser,
     isLoadingUser: isLoading,
     isError: error,
-    id: parseInt(storedId),
+    id: parseInt(loggedInUserId as string),
   };
 };
