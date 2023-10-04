10973289ceec70b2f8896bc65a60190cddd4f145
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2023-10-04 16:59:22 -0400

    Styles main page for alpha release

---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+import styles from "./footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.container}>
+      <div className={styles.topSection}>
+        <div className={styles.rowItem}>
+          <h2>About Us</h2>
+          <p>
+            Learn more about our mission and vision to connect communities
+            abroad.
+          </p>
+        </div>
+
+        <div className={styles.rowItem}>
+          <h2>Contact</h2>
+          <p>
+            Get in touch with our support team for any inquiries or assistance.
+          </p>
+        </div>
+
+        <div className={styles.rowItem}>
+          <h2>FAQs</h2>
+          <p>
+            Find answers to common questions about our platform and services.
+          </p>
+        </div>
+
+        <div className={styles.rowItem}>
+          <h2>Legal</h2>
+          <ul>
+            <li>
+              <a href="#">Terms of Use</a>
+            </li>
+            <li>
+              <a href="#">Privacy Policy</a>
+            </li>
+          </ul>
+        </div>
+
+        <div className={styles.rowItem}>
+          <h2>Blog</h2>
+          <p>
+            Read our latest articles and updates on community building and
+            travel.
+          </p>
+        </div>
+
+        <div className={styles.rowItem}>
+          <h2>Careers</h2>
+          <p>
+            Explore job opportunities and join our team to make a global impact.
+          </p>
+        </div>
+
+        <div className={styles.rowItem}>
+          <h2>Press Room</h2>
+          <p>Stay updated with our latest news and press releases.</p>
+        </div>
+      </div>
+
+      <div className={styles.bottomSection}>
+        <p>&copy; 2023 Your Company Name. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,15 +1,28 @@
 "use client";
 import styles from "./navbarstyles.module.scss";
-import LogoutBtn from "../logout/logoutBtn";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
-    <div className={styles.container}>
-      <div>
-        <h1>Nomax</h1>
+    <>
+      <div className={styles.container}>
+        <div className={styles.psuedoContainer} />
+        <div className={styles.logoContainer}>
+          <Image
+            className={styles.logoImg}
+            src="https://res.cloudinary.com/dvz91qyth/image/upload/v1696446061/Nomex/dashboard/travel_chxryf.png"
+            width={50}
+            height={50}
+            alt="logo"
+          />
+          <h1>Nomax</h1>
+        </div>
+        <div className={styles.btnContainer}>
+          <Link href="/blog">Blog</Link>
+          <Link href="/auth">Join</Link>
+        </div>
       </div>
-      <div className={styles.btnContainer}></div>
-      <LogoutBtn />
-    </div>
+    </>
   );
 }
